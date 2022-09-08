de413bfbd71eff3b2d4db1cb30f1a67d9a3c40d9
nhandt <nhandt@nexlesoft.com>
2022-09-08 18:34:24 +0700

    implement run workflow API

--- index.js
+++ index.js
@@ -36,45 +36,51 @@ app.get("/workflow/:id", cors(), async (req, res) => {
   const data = await response.json();
   console.log("Result =====", data, req.params);
 
-  res.send(data);
+  return res.send(data);
 });
 
 app.post("/workflow", cors(), async (req, res) => {
   const { url } = req.body;
 
-  const response = await fetch("https://play.orkes.io/api/workflow", {
-    headers: {
-      accept: "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "cache-control": "no-cache",
-      "content-type": "application/json",
-      pragma: "no-cache",
-      "sec-ch-ua":
-        '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"macOS"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-authorization": req.headers["x-authorization"],
-    },
-    referrer: "https://play.orkes.io/",
-    referrerPolicy: "strict-origin",
-    body: JSON.stringify({
-      name: "VisualImageSearch",
-      version: "13",
-      correlationId: "",
-      input: { imageUrl: url },
-    }),
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-  });
+  if (url) {
+    const response = await fetch("https://play.orkes.io/api/workflow", {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-authorization": req.headers["x-authorization"],
+      },
+      referrer: "https://play.orkes.io/",
+      referrerPolicy: "strict-origin",
+      body: JSON.stringify({
+        name: "VisualImageSearch",
+        version: "13",
+        correlationId: "",
+        input: { imageUrl: url },
+      }),
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    });
 
-  const data = await response.text();
-  console.log("Workflow Result =====", data, url);
+    const data = await response.text();
+    console.log("Workflow Result =====", data, url);
 
-  res.send(data);
+    return res.send(data);
+  }
+
+  console.log("Workflow Bad request =====", req.body);
+
+  return res.sendStatus(400);
 });
 
 app.listen(port, () =>
