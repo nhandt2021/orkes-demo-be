b67efd90a63d81a936c239a84643bbd6402439db
nhandt <nhandt@nexlesoft.com>
2022-09-08 23:10:26 +0700

    implement run video workflow API

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import fetch from "node-fetch";
+import md5 from "md5";
 
 const app = express();
 const port = 3000;
 
-// Where we will keep books
-let books = [];
+const BASE_URL = "https://play.orkes.io/api";
 
 app.use(cors());
 
@@ -20,7 +20,7 @@ app.get("/workflow/:id", cors(), async (req, res) => {
   const { id } = req.params;
 
   // https://play.orkes.io/execution/f9a9d984-2e51-11ed-85d6-da345edc3cc9?tabIndex=4
-  const response = await fetch(`https://play.orkes.io/api/workflow/${id}`, {
+  const response = await fetch(`${BASE_URL}/workflow/${id}`, {
     headers: {
       accept: "*/*",
       "sec-fetch-mode": "cors",
@@ -34,16 +34,52 @@ app.get("/workflow/:id", cors(), async (req, res) => {
   });
 
   const data = await response.json();
-  console.log("Result =====", data, req.params);
+  // console.log("Result =====", data, req.params);
 
   return res.send(data);
 });
+
+const getWorkflowByCorrelationId = async ({
+  workflowName,
+  correlationId,
+  token,
+}) => {
+  const response = await fetch(
+    `${BASE_URL}/workflow/${workflowName}/correlated/${correlationId}`,
+    {
+      headers: {
+        "x-authorization": token,
+      },
+      referrer: "https://play.orkes.io/",
+      referrerPolicy: "strict-origin",
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+    }
+  );
+
+  const data = await response.json();
+  console.log("getWorkflowByCorrelationId Result =====", data);
+
+  return data;
+};
 
 app.post("/workflow", cors(), async (req, res) => {
   const { url } = req.body;
 
   if (url) {
-    const response = await fetch("https://play.orkes.io/api/workflow", {
+    const urlMD5 = md5(url);
+    const workflows = await getWorkflowByCorrelationId({
+      workflowName: "VisualImageSearch",
+      correlationId: urlMD5,
+      token: req.headers["x-authorization"],
+    });
+
+    if (Array.isArray(workflows) && workflows.length) {
+      return res.send(workflows[0].workflowId);
+    }
+
+    const response = await fetch(`${BASE_URL}/workflow`, {
       headers: {
         accept: "*/*",
         "accept-language": "en-US,en;q=0.9",
@@ -64,7 +100,7 @@ app.post("/workflow", cors(), async (req, res) => {
       body: JSON.stringify({
         name: "VisualImageSearch",
         version: "13",
-        correlationId: "",
+        correlationId: urlMD5,
         input: { imageUrl: url },
       }),
       method: "POST",
@@ -73,12 +109,12 @@ app.post("/workflow", cors(), async (req, res) => {
     });
 
     const data = await response.text();
-    console.log("Workflow Result =====", data, url);
+    // console.log("Workflow Result =====", data, url);
 
     return res.send(data);
   }
 
-  console.log("Workflow Bad request =====", req.body);
+  // console.log("Workflow Bad request =====", req.body);
 
   return res.sendStatus(400);
 });
@@ -87,7 +123,18 @@ app.post("/videoWorkflow", cors(), async (req, res) => {
   const { url } = req.body;
 
   if (url) {
-    const response = await fetch("https://play.orkes.io/api/workflow", {
+    const urlMD5 = md5(url);
+    const workflows = await getWorkflowByCorrelationId({
+      workflowName: "video_thumbnail_generator",
+      correlationId: urlMD5,
+      token: req.headers["x-authorization"],
+    });
+
+    if (Array.isArray(workflows) && workflows.length) {
+      return res.send(workflows[0].workflowId);
+    }
+
+    const response = await fetch(`${BASE_URL}/workflow`, {
       headers: {
         accept: "*/*",
         "accept-language": "en-US,en;q=0.9",
@@ -108,7 +155,7 @@ app.post("/videoWorkflow", cors(), async (req, res) => {
       body: JSON.stringify({
         name: "video_thumbnail_generator",
         version: "1",
-        correlationId: "",
+        correlationId: urlMD5,
         input: {
           fileLocation: url,
           outputFileFormat: "png",
@@ -125,7 +172,7 @@ app.post("/videoWorkflow", cors(), async (req, res) => {
     return res.send(data);
   }
 
-  console.log("videoWorkflow Bad request =====", req.body);
+  // console.log("videoWorkflow Bad request =====", req.body);
 
   return res.sendStatus(400);
 });
